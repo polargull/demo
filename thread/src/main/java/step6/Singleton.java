c318a53ef0fe6437ba8c1989ada35c8c8338fa51
polargull <fuwei04ruanjian@gmail.com>
2017-01-07 20:10:52 +0800

    创建单例添加外层非空判断

--- thread/src/main/java/step6/Singleton.java
+++ thread/src/main/java/step6/Singleton.java
@@ -4,23 +4,25 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by fuyuanpu on 2017/1/7.
- * 线程安全的单例
+ * 线程安全的单例 DCL
  */
 public class Singleton {
     private static Singleton ourInstance;
     private static ReentrantLock lock = new ReentrantLock();
     public static Singleton getInstance() {
-        lock.lock();
         if (ourInstance == null) {
-            //测试使用
+            lock.lock();
+            if (ourInstance == null) {
+                //测试使用
 //            try {
 //                Thread.sleep(3000);
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
-            ourInstance = new Singleton();
+                ourInstance = new Singleton();
+            }
+            lock.unlock();
         }
-        lock.unlock();
         return ourInstance;
     }
 
