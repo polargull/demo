86a52f4bf856423e405e259ed8294708ee68a0c6
polargull <fuwei04ruanjian@gmail.com>
2017-01-07 10:35:02 +0800

    thread demo

---
+++ thread/src/main/java/step1/Thread1.java
@@ -0,0 +1,26 @@
+package step1;
+
+/**
+ * Created by fuyuanpu on 2017/1/7.
+ */
+public class Thread1 {
+    public static void main(String[] args) {
+        System.out.println("当前线程名：" + Thread.currentThread().getName());
+        new MyThread1().start();
+        new Thread(new MyThread2()).start();
+    }
+
+    public static class MyThread1 extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            System.out.println("My Thread1!");
+        }
+    }
+
+    public static class MyThread2 implements Runnable {
+        public void run() {
+            System.out.println("My Thread2!");
+        }
+    }
+}
