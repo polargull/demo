bad53ce3e6a69a16bae2faf188cca76b5d6fe787
fu.wei <wei.fu@mtime.com>
2017-01-25 18:09:01 +0800

    注入velocity引擎

--- boot/src/main/java/boot/ApplicationBootstrap.java
+++ boot/src/main/java/boot/ApplicationBootstrap.java
@@ -1,7 +1,11 @@
 package boot;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by fuyuanpu on 2017/1/14.
@@ -10,5 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApplicationBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationBootstrap.class, args);
+    }
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
+        return ve;
     }
 }
--- boot/src/main/java/boot/web/FormController.java
+++ boot/src/main/java/boot/web/FormController.java
@@ -1,15 +1,29 @@
 package boot.web;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.StringWriter;
 
 /**
  * Created by fuyuanpu on 2017/1/15.
  */
 @Controller
 public class FormController {
+    @Autowired
+    VelocityEngine ve;
+
     @RequestMapping("index")
     public String index() {
+        Template t = ve.getTemplate("/view/form.vm");
+        VelocityContext ctx = new VelocityContext();
+        StringWriter sw = new StringWriter();
+        t.merge(ctx, sw);
+        System.out.println(sw.toString());
         return "view/form";
     }
 }
